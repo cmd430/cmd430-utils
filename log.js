c3189fb2aab4329bfe807e17693bddc5140c649d
cmd430 <cmd430@gmail.com>
2023-03-27 01:23:54 +1300

    padCenter log tags by default with option to disable

--- log.js
+++ log.js
@@ -2,7 +2,7 @@ import { grey, red, yellow, magenta, cyan, white } from './colors.js'
 import { strip } from './formatting.js'
 import { timestamp as currentTimestamp } from './_internal/timestamp.js'
 import { parse } from './_internal/parse.js'
-import { isString, isObject, isDevEnv } from './misc.js'
+import { isString, isObject, isDevEnv, padCenter } from './misc.js'
 
 const timestamp = () => grey(`[${currentTimestamp()}]`)
 const filterMessage = args => args.filter(a => a)
@@ -15,17 +15,20 @@ export class Log {
   /* eslint-disable lines-between-class-members */
   #Log = Log
   #tag
+  #tagCenter
   #tagDevOnly
   #formatting
   /* eslint-enable lines-between-class-members */
 
   /**
    * @constructor
-   * @param {Object|String} [tagOpts] - String or an Object with log tag options
-   * @param {Object|String} [tagOpts.tag=undefind] - String to tag log messages with or log tag options
+   * @param {Object} [tagOpts] - String tag or Object with log tag options
+   * @param {Object} [tagOpts.tag=undefind] - String to tag log messages with or log tag options
    * @param {String} [tagOpts.tag.text=undefind] - String to tag log messages with
+   * @param {String} [tagOpts.tag.center=true] - String to tag log messages with
    * @param {Boolean} [tagOpts.tag.devOnly=false] - If tag should only been shown in dev mode
    * @param {Boolean} [tagOpts.formatting=true] - If logs should be have colors and styles enabled
+   * @param {String} [tagOpts] - String tag
    */
   constructor (tagOpts) {
     this.debug = this.debug.bind(this)
@@ -39,16 +42,20 @@ export class Log {
       if (isString(tagOpts.tag)) this.#tag = tagOpts.tag?.trim()
       if (isObject(tagOpts.tag)) {
         this.#tag = tagOpts.tag?.text?.trim()
+        this.#tagCenter = tagOpts.tag?.center ?? true
         this.#tagDevOnly = tagOpts.tag?.devOnly ?? false
       }
       this.#formatting = tagOpts.formatting ?? true
     }
-    if (this.#tag && this.#tag.length + 2 > Log.#maxTagLength) Log.#maxTagLength = this.#tag.length + 2
+    if (this.#tag) this.#tag = `[ ${this.#tag} ]`
+    if (this.#tag && this.#tag.length > Log.#maxTagLength) Log.#maxTagLength = this.#tag.length
   }
 
   #logTag () {
     if (this.#tagDevOnly === true && isDevEnv() === false) return
-    return this.#tag ? white(paddedTag(`[${this.#tag}]`, this.#Log.#maxTagLength)) : paddedTag('', this.#Log.#maxTagLength)
+    if (this.#tagCenter) return this.#tag ? white(padCenter(this.#tag, this.#Log.#maxTagLength)) : paddedTag('', this.#Log.#maxTagLength)
+
+    return this.#tag ? white(paddedTag(this.#tag, this.#Log.#maxTagLength)) : paddedTag('', this.#Log.#maxTagLength)
   }
 
   #msg (type, color, ...args) {
