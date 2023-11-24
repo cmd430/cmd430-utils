5b8070d22c615b52adf1c0ad8b6c8d5ca0d77298
cmd430 <cmd430@gmail.com>
2023-11-24 22:57:04 +1300

    dont inspect errors unless we are showing hidden properties

--- _internal/parse.js
+++ _internal/parse.js
@@ -2,7 +2,9 @@ import { inspect } from 'node:util'
 import { isArray, isObject, isDate, isError } from '../misc.js'
 
 export const parse = ({ colors, showHidden }, ...args) => args.map(arg => {
-  if (isObject(arg) || isArray(arg) || isDate(arg) || isError(arg)) return inspect(arg, {
+  const shouldInspect = (isObject(arg) || isArray(arg) || isDate(arg))
+
+  if (shouldInspect || (showHidden && isError(arg))) return inspect(arg, {
     showHidden: showHidden,
     depth: null,
     colors: colors
