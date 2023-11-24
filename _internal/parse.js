810b067047c556871f3028bb90d7e4272ad84875
cmd430 <cmd430@gmail.com>
2023-11-24 22:48:30 +1300

    allow show hidden properties on errors

--- _internal/parse.js
+++ _internal/parse.js
@@ -1,8 +1,8 @@
 import { inspect } from 'node:util'
-import { isArray, isObject, isDate } from '../misc.js'
+import { isArray, isObject, isDate, isError } from '../misc.js'
 
 export const parse = ({ colors, showHidden }, ...args) => args.map(arg => {
-  if (isObject(arg) || isArray(arg) || isDate(arg)) return inspect(arg, {
+  if (isObject(arg) || isArray(arg) || isDate(arg) || isError(arg)) return inspect(arg, {
     showHidden: showHidden,
     depth: null,
     colors: colors
