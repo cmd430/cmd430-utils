9fedd1a642bb281731236683aca092d79b7275ce
cmd430 <cmd430@gmail.com>
2024-01-08 20:37:41 +1300

    add stdout.hasColors() check if false then we do extra checks

--- _internal/supports.js
+++ _internal/supports.js
@@ -1,8 +1,9 @@
-import { env, platform } from 'node:process'
+import { env, platform, stdout } from 'node:process'
 
 export function formatting () {
   // super basic color support check
 
+  if (stdout.hasColors()) return true
   if ((/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i).test(env.TERM)) return true
   if ('COLORTERM' in env) return true
   if (platform === 'win32') return true
