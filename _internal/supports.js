9bed49a3ac65dfe294294d7b4e6815af0f7eb2e3
cmd430 <cmd430@gmail.com>
2024-02-02 00:07:09 +1300

    remove debug msg

--- _internal/supports.js
+++ _internal/supports.js
@@ -3,8 +3,6 @@ import { env, platform, stdout } from 'node:process'
 export function formatting () {
   // super basic color support check
 
-  console.debug(env)
-
   if ((/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i).test(env.TERM)) return true
   if ('COLORTERM' in env) return true
   if (platform === 'win32') return true
