42a18d56fdb44106f33dfe7da46c579f03135208
cmd430 <cmd430@gmail.com>
2024-02-01 23:58:46 +1300

    adjust order of color term detection to potentially fix podman container issues

--- _internal/supports.js
+++ _internal/supports.js
@@ -3,10 +3,10 @@ import { env, platform, stdout } from 'node:process'
 export function formatting () {
   // super basic color support check
 
-  if (stdout.hasColors()) return true
   if ((/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i).test(env.TERM)) return true
   if ('COLORTERM' in env) return true
   if (platform === 'win32') return true
+  if (stdout.hasColors()) return true
 
   return false
 }
