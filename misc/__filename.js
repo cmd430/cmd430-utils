c2a4ac48491a05662eaa6cc8562be8162739d8c9
cmd430 <cmd430@gmail.com>
2024-04-03 02:46:36 +1300

    support bun

--- misc/__filename.js
+++ misc/__filename.js
@@ -3,7 +3,8 @@ import { callsites as getCallsites } from '../_internal/callsites.js'
 
 // eslint-disable-next-line no-underscore-dangle
 export function __filename ({ depth } = { depth: 0 }) {
-  const callsites = getCallsites().filter(c => c.getFileName()?.startsWith('file:///'))
+  const callsites = getCallsites().filter(c => c.getFileName().startsWith('node:internal') === false && c.getFileName().length > 0)
+  const callsite = callsites[callsites.length - (1 + (depth > -1 ? depth : 0))].getFileName()
 
-  return fileURLToPath(callsites[callsites.length - (1 + (depth > -1 ? depth : 0))].getFileName())
+  return fileURLToPath(callsite.startsWith('file:///') ? callsite : `file:///${callsite}`)
 }
