35f1dd7d996d506e5ca9427702614e35282badbb
cmd430 <cmd430@gmail.com>
2025-04-05 06:11:43 +1300

    cleaner __filename for bun and node

--- misc/__filename.js
+++ misc/__filename.js
@@ -3,7 +3,7 @@ import { callsites as getCallsites } from '../_internal/callsites.js'
 
 // eslint-disable-next-line no-underscore-dangle
 export function __filename ({ depth } = { depth: 0 }) {
-  const callsites = getCallsites().filter(c => c.getFileName().startsWith('node:internal') === false && c.getFileName().length > 0)
+  const callsites = getCallsites().filter(c => !c.isNative() && !c.getFileName().startsWith('node:internal'))
   const callsite = callsites[callsites.length - (1 + (depth > -1 ? depth : 0))].getFileName()
 
   return fileURLToPath(callsite.startsWith('file:///') ? callsite : `file:///${callsite}`)
